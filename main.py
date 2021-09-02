558cfe7ef330fb25805b4e25c49a97065fa2ee0b
Kodow1 <89237187+Kodow1@users.noreply.github.com>
2021-09-02 11:24:03 +1000

    added walls to create maze in 3 rooms, added collisions with walls, created one random wall
    I added walls to rooms 2a,2b, and 3 using the Walls class. Move function in Player has been changed to collide with walls.
    The random wall spawns 1 of 2 walls to block one of the 2 exits in the maze randomly.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 pygame.display.set_caption("BOB the Blob")
@@ -28,7 +29,7 @@ class Doors(pygame.sprite.Sprite):
         self.name = name
         self.rect = pygame.Rect(x,y,self.width,self.height)
 
-class Wall(pygame.sprite.Sprite):
+class Walls(pygame.sprite.Sprite):
     def __init__(self,x,y,width,height,colour):
         super().__init__()
         self.x = x
@@ -44,17 +45,37 @@ class Player(pygame.sprite.Sprite):
         self.width,self.height = self.image.get_rect().size
         self.rect = pygame.Rect(x,y,self.width,self.height)
         self.velocity = 7
+        self.change_x = 0
+        self.change_y = 0
+        self.rect_vel = pygame.Rect(x-self.velocity,y-self.velocity,self.width+(self.velocity*2),self.height+(self.velocity*2))
         
     def move(self,keys_pressed,room):
         if keys_pressed[pygame.K_a] and self.rect.x - self.velocity > room.left or keys_pressed[pygame.K_LEFT] and self.rect.x - self.velocity > room.left:  # LEFT
-            self.rect.x -= self.velocity
+            self.change_x = (-1*self.velocity)
         if keys_pressed[pygame.K_d] and self.rect.x + self.velocity + self.width < room.right or keys_pressed[pygame.K_RIGHT] and self.rect.x + self.velocity + self.width < room.right:  # RIGHT
-            self.rect.x += self.velocity
+            self.change_x = self.velocity
+        self.rect.x += self.change_x
+        block_hit_list = pygame.sprite.spritecollide(self, room.wall_list, False)
+        for block in block_hit_list:
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            else:
+                self.rect.left = block.rect.right
+        
         if keys_pressed[pygame.K_w] and self.rect.y - self.velocity > room.top or keys_pressed[pygame.K_UP] and self.rect.y - self.velocity > room.top:  # UP
-            self.rect.y -= self.velocity
+            self.change_y = (-1*self.velocity)
         if keys_pressed[pygame.K_s] and self.rect.y + self.velocity + self.height < room.bottom or  keys_pressed[pygame.K_DOWN] and self.rect.y + self.velocity + self.height < room.bottom:  # DOWN
-            self.rect.y += self.velocity  
-   
+            self.change_y = self.velocity  
+        self.rect.y += self.change_y    
+        block_hit_list = pygame.sprite.spritecollide(self, room.wall_list, False)
+        for block in block_hit_list:
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            else:
+                self.rect.top = block.rect.bottom
+        self.change_x = 0
+        self.change_y = 0
+
 class Room():
     def __init__(self,name,image,doors,walls):
         super().__init__()
@@ -62,13 +83,18 @@ class Room():
         self.image = pygame.image.load(image)
         self.top = 200
         self.bottom = HEIGHT-10
-        self.left = 13
-        self.right = WIDTH - 13
+        self.left = 5
+        self.right = WIDTH - 5
         self.door_list = pygame.sprite.Group()
-        
+        self.wall_list = pygame.sprite.Group()
+        self.walls = walls
         for item in doors:
             door = Doors(item[0], item[1], item[2], item[3], item[4])
             self.door_list.add(door)
+        if walls != []:
+            for item in walls:
+                wall = Walls(item[0], item[1], item[2], item[3], item[4])
+                self.wall_list.add(wall)
 
 def room_change(player,room):
     """if player is colliding with door, move to the next room based on the .name of the door."""
@@ -109,22 +135,34 @@ def room_change(player,room):
                 if room == room2b: 
                     player.rect.x = spawn_x_left 
     return new_room
-    
+
 def draw_window(player,room):
     SCREEN.blit(room.image, (0,0))
     SCREEN.blit(player.image, (player.rect.x, player.rect.y))
     # for door in room.door_list: # un-code-comment to show doors
     #     pygame.draw.rect(SCREEN,(255,255,255),door.rect)
+    if room.walls != []:
+        for wall in room.wall_list:
+            pygame.draw.rect(SCREEN,wall.colour,wall.rect)
     pygame.display.update()
+#walls (roomname_ws) ws = walls
 
+if random.randint(1,2) == 1:
+    stopper_wall = [248,435,22,127,LEAVES]
+else:
+    stopper_wall = [248,540,22,127,LEAVES]
+
+room2a_ws = [[450,294,650,22,LEAVES],[590,294,22,110,LEAVES],[1078,294,22,178,LEAVES],[681,382,336,22,LEAVES],[681,382,22,126,LEAVES],[450,487,253,22,LEAVES],[450,628,650,22,LEAVES],[750,472,22,178,LEAVES],[915,472,22,178,LEAVES],[832,382,22,180,LEAVES],[996,382,22,180,LEAVES],[996,543,104,22,LEAVES]]
+room2b_ws = [[0,294,22,180,LEAVES],[0,294,291,22,LEAVES],[0,540,112,22,LEAVES],[0,628,1100,22,LEAVES],[90,382,22,180,LEAVES],[90,382,818,22,LEAVES],[270,228,830,22,LEAVES],[270,228,22,88,LEAVES],[179,460,296,22,LEAVES],[355,323,22,81,LEAVES],[441,305,553,22,LEAVES],[541,382,22,190,LEAVES],[270,460,22,103,LEAVES],[179,540,112,22,LEAVES],[179,540,22,100,LEAVES],[366,549,22,100,LEAVES],[453,549,22,109,LEAVES],[542,382,22,190,LEAVES],[542,552,102,22,LEAVES],[636,475,103,22,LEAVES],[717,382,22,268,LEAVES],[886,382,22,92,LEAVES],[808,464,22,98,LEAVES],[808,541,292,22,LEAVES],[972,305,22,257,LEAVES],[1078,229,22,230,LEAVES]]
+room3_ws = [[0,229,22,228,LEAVES],[0,435,270,22,LEAVES],[0,540,270,22,LEAVES],[0,630,270,22,LEAVES],stopper_wall]
 #rooms
-room1 = Room("room1","backgrounds/BG-1.png",[[59,210,94,67,"1a"],[248,210,94,67,"1b"],[445,210,94,67,"1c"],[1060,0,40,650,"2a"]],[])
-room1a = Room("room1a","backgrounds/BG-1-A.png",[[500,635,100,40,"1"]],[])        
-room1b = Room("room1b","backgrounds/BG-1-B.png",[[500,635,100,40,"1"]],[])
-room1c = Room("room1c","backgrounds/BG-1-C.png",[[500,635,100,40,"1"]],[])
-room2a = Room("room2a","backgrounds/BG-2-A.png",[[0,0,40,650,"1"],[1060,0,40,650,"2b"]],[])
-room2b = Room("room2b","backgrounds/BG-2-B.png",[[0,0,40,650,"2a"],[1060,0,40,650,"3"]],[])
-room3  = Room("room3","backgrounds/BG-3.png",[[0,0,40,650,"2b"]],[])
+room1 = Room("room1","backgrounds/BG-1.png",[[59,210,94,67,"1a"],[248,210,94,67,"1b"],[445,210,94,67,"1c"],[1085,0,15,650,"2a"]],[])
+room1a = Room("room1a","backgrounds/BG-1-A.png",[[500,635,100,15,"1"]],[])        
+room1b = Room("room1b","backgrounds/BG-1-B.png",[[500,635,100,15,"1"]],[])
+room1c = Room("room1c","backgrounds/BG-1-C.png",[[500,635,100,15,"1"]],[])
+room2a = Room("room2a","backgrounds/BG-2-A.png",[[0,0,15,650,"1"],[1085,0,15,650,"2b"]],room2a_ws)
+room2b = Room("room2b","backgrounds/BG-2-B.png",[[0,0,15,650,"2a"],[1085,0,15,650,"3"]],room2b_ws)
+room3  = Room("room3","backgrounds/BG-3.png",[[0,0,15,650,"2b"]],room3_ws)
 
 spawn_x_left = 50
 spawn_x_right = WIDTH - (100)
