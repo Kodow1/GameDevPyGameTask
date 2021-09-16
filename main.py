4f8e5ddcc5045be7c4aa08fc5d23be17266c981e
Kodow1 <89237187+Kodow1@users.noreply.github.com>
2021-09-16 10:30:55 +1000

    Final polishing and editing

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import pygame
-import random
-import time
-from pygame.constants import MOUSEBUTTONDOWN
-
-from pygame.key import name
-
 '''
 This Code was made largely by myself, Oliver Waldock. In saying that 
 I did use pieces of others work. If I have used others code I have refrenced
 it with the code taken.
+
+Music copyright disclaimer:
+In my dreams - Fato Shadow
+Paradise - Fato Shadow
+bossfight - public domain
+triamphant - public domain
 '''
 
+import pygame
+import random
+import time
+
 pygame.init()
-font = pygame.font.init()
 pygame.mixer.init()
 pygame.display.set_caption("BOB the Blob")
 
+#Screen
 WIDTH,HEIGHT = 1100,650
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 FPS = 60
@@ -29,7 +32,6 @@ WATER = (186,206,243)
 WHITE = (255,255,255)
 D_GREY = (100,100,100)
 BLACK = (0,0,0)
-
 
 class Doors(pygame.sprite.Sprite):
     '''Doors are created and used to move the player to adjacent rooms. 
@@ -56,6 +58,7 @@ class Walls(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x,y,self.width,self.height)
         
 class Interactable(pygame.sprite.Sprite):
+    """Interactables are sprites that the player can interacte with (NPC's and Buttons)"""
     def __init__(self,name,x,y,image):
         super().__init__()
         self.x = x
@@ -107,17 +110,6 @@ class Player(pygame.sprite.Sprite):
         self.change_x = 0
         self.change_y = 0
 
-# class NPC(pygame.sprite.Sprite):
-#     def __init__(self,name,x,y,image):
-#         super().__init__()
-#         self.x = x
-#         self.y = y
-#         self.image = pygame.image.load(image)
-#         self.name = name
-#         self.width,self.height = self.image.get_rect().size
-#         self.rect = pygame.Rect(x,y,self.width,self.height)
-#         pass
-
 class Room():
     '''Creates a room, stores doors and walls'''
     def __init__(self,name,image,doors,border,walls,interactables):
@@ -143,6 +135,12 @@ class Room():
             for item in interactables:
                 interactables = Interactable(item[0], item[1], item[2], item[3])
                 self.inter_list.add(interactables)    
+
+class HealthBar():
+    """Creates a health bar displayed on the top right"""
+    def __init__(self):
+        self.health = 0
+        self.images = [pygame.image.load("ASSESTS/Health-100.png"),pygame.image.load("ASSESTS/Health-50.png"),pygame.image.load("ASSESTS/Health-0.png")]
 
 maze_liabry = {1:[
     #  x  0   1   2   3   4   5   6   7   8   9   10  11  12  13    y
@@ -179,22 +177,11 @@ maze_liabry = {1:[
         ["T",".","l","T","-","T","-","l",".","T",".","l","-","l"], #6
         ["T","T",".",".","l",".","l","l","-",".","T","-","l","l"], #7
         ["l",".","T","-",".","T",".","l","-","-","-",".","l","."], #8
-        ["-","-","-","-","-","-","-","-","-","-","-","-","-","-"]], #9
-               4:[
-    #  x  0   1   2   3   4   5   6   7   8   9   10  11  12  13    y
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #0
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #1
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #2
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #3
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #4
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #5
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #6
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #7
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."], #8
-        [".",".",".",".",".",".",".",".",".",".",".",".",".","."]] #9
+        ["-","-","-","-","-","-","-","-","-","-","-","-","-","-"]] #9
                }    
 
-def create_maze():
+def create_maze(maze_num):
+    """Turns mazes from maze liabry into a list of walls"""
     x_c = 84
     y_c = 71
     vertical = (10,71)
@@ -203,8 +190,7 @@ def create_maze():
     width = 14 
     height = 10
     walls = []
-    maze_grid = maze_liabry[3] 
-    #random.randint(1,2)
+    maze_grid = maze_liabry[maze_num] 
     for x in range(0,width):
         for y in range(0,height):
             if maze_grid[y][x] == ".":
@@ -273,13 +259,18 @@ def room_change(player,room):
                 boss.fadeout(50)
                 triumphant.play(loops=100, maxtime=0, fade_ms=50)
                 player.rect.x,player.rect.y = spawn_x_left,spawn_y_center
+            elif door.name == "endscreen":
+                new_room = endscreen
     return new_room
 
 def draw_window(player,room):
     '''Draws display on screen'''
     SCREEN.blit(room.image, (0,0))
-    if room != homescreen:
-        SCREEN.blit(player.image, (player.rect.x, player.rect.y))
+    # if room != homescreen or room != endscreen:
+    #     SCREEN.blit(player.image, (player.rect.x, player.rect.y))
+    SCREEN.blit(player.image, (player.rect.x, player.rect.y))
+    if dialog_tally > 0:
+        SCREEN.blit(health_bar.images[health_bar.health], (825, 16))
     for NPC in room.inter_list:
         SCREEN.blit(NPC.image, (NPC.rect.x, NPC.rect.y))
     # for door in room.door_list: # un-code-comment to show doors
@@ -287,52 +278,46 @@ def draw_window(player,room):
     if room.walls != []:
         for wall in room.wall_list:
             pygame.draw.rect(SCREEN,wall.colour,wall.rect)
-    # SCREEN.blit(text_image,text_rect)
     pygame.display.update()
 
-#dialog
-cole0 = ["ASSESTS/1-0.png"]
-floyd_blue = ["ASSESTS/0-0.png","ASSESTS/0-1.png","ASSESTS/0-2.png","ASSESTS/0-3.png"]
-cole1 = ["ASSESTS/1-0.png","ASSESTS/1-1.png","ASSESTS/1-2.png","ASSESTS/1-3.png","ASSESTS/1-4.png","ASSESTS/1-5.png","ASSESTS/1-6.png","ASSESTS/1-7.png","ASSESTS/1-8.png","ASSESTS/1-9.png","ASSESTS/1-10.png"]
-blue1 = ["ASSESTS/0-1.png"]
-pink1 = ["ASSESTS/p-1.png"]
-pink2 = ["ASSESTS/p-2.png"]
-cole2 = ["ASSESTS/c-2.png"]
-pygame.display.update()
-def dialog(d_t,name):
+def dialog(d_t,interactable,room):
+    """This finds what dialog needs to be asscess depending on how are the player has played which is kept track by dialog_tally or d_t"""
     in_dialog = True
-    if d_t == 0:
-        if name =="Cole":
+    if interactable.name == "flower":
+        list = ["ASSESTS/flower-1.png"]
+        room.inter_list.remove(interactable)
+        health_bar.health = 2
+    elif interactable.name == "petal":
+        list = ["ASSESTS/petal-1.png"]
+        room.inter_list.remove(interactable)
+        health_bar.health = 1
+    elif d_t == 0:
+        if interactable.name =="Cole":
             list = ["ASSESTS/1-0.png"]
         else:
             list = ["ASSESTS/0-0.png","ASSESTS/0-1.png","ASSESTS/0-2.png","ASSESTS/0-3.png"]
             d_t +=1
     elif d_t == 1:
-        if name =="Cole":
+        if interactable.name =="Cole":
             list = ["ASSESTS/1-0.png","ASSESTS/1-1.png","ASSESTS/1-2.png","ASSESTS/1-3.png","ASSESTS/1-4.png","ASSESTS/1-5.png","ASSESTS/1-6.png","ASSESTS/1-7.png","ASSESTS/1-8.png","ASSESTS/1-9.png","ASSESTS/1-10.png"]
             d_t +=1
-        elif name == "Mr.Blue":
+        elif interactable.name == "Mr.Blue":
             list = ["ASSESTS/0-1.png"]
         else:
             list = ["ASSESTS/p-1.png"]
     else:
-        if name =="Cole":
+        if interactable.name =="Cole":
             list = ["ASSESTS/c-2.png"]
-        elif name == "Mr.Blue":
+        elif interactable.name == "Mr.Blue":
             list = ["ASSESTS/0-1.png"]
         else:
             list = ["ASSESTS/p-2.png"]
-    return in_dialog,list,d_t
-    
-        
+    return in_dialog,list,d_t 
 
-if True: # just to allow collapsing 
+if True: # Stores all base varibales in a collapsible state
     s_border = (200,HEIGHT-5,5,WIDTH - 5)
     f_border = (3,HEIGHT-3,3,WIDTH - 3)
 
-    # font = pygame.font.Font(None, 36)
-    # text_image = font.render("Testing 123", True, (255, 255, 255))
-    # text_rect = text_image.get_rect()
 
     #walls (roomname_ws) ws = walls
 
@@ -342,16 +327,21 @@ if True: # just to allow collapsing
         stopper_wall = [248,540,22,127,LEAVES]
 
 
-    room2a_ws = [[450,294,650,22,LEAVES],[590,294,22,110,LEAVES],[1078,294,22,178,LEAVES],[681,382,336,22,LEAVES],[681,382,22,126,LEAVES],[450,487,253,22,LEAVES],[450,628,650,22,LEAVES],[750,472,22,178,LEAVES],[915,472,22,178,LEAVES],[832,382,22,180,LEAVES],[996,382,22,180,LEAVES],[996,543,104,22,LEAVES]]
+    room2a_ws = [[590,294,510,22,LEAVES],[590,294,22,110,LEAVES],[1078,294,22,178,LEAVES],[681,382,336,22,LEAVES],[681,382,22,126,LEAVES],[450,487,253,22,LEAVES],[450,628,650,22,LEAVES],[750,472,22,178,LEAVES],[915,472,22,178,LEAVES],[832,382,22,180,LEAVES],[996,382,22,180,LEAVES],[996,543,104,22,LEAVES]]
     room2b_ws = [[0,294,22,180,LEAVES],[0,294,291,22,LEAVES],[0,540,112,22,LEAVES],[0,628,1100,22,LEAVES],[90,382,22,180,LEAVES],[90,382,818,22,LEAVES],[270,228,830,22,LEAVES],[270,228,22,88,LEAVES],[179,460,296,22,LEAVES],[355,323,22,81,LEAVES],[441,305,553,22,LEAVES],[541,382,22,190,LEAVES],[270,460,22,103,LEAVES],[179,540,112,22,LEAVES],[179,540,22,100,LEAVES],[366,549,22,100,LEAVES],[453,549,22,109,LEAVES],[542,382,22,190,LEAVES],[542,552,102,22,LEAVES],[636,475,103,22,LEAVES],[717,382,22,268,LEAVES],[886,382,22,92,LEAVES],[808,464,22,98,LEAVES],[808,541,292,22,LEAVES],[972,305,22,257,LEAVES],[1078,229,22,230,LEAVES]]
     room3_ws = [[0,229,22,228,LEAVES],[0,435,270,22,LEAVES],[0,540,270,22,LEAVES],[0,630,270,22,LEAVES],stopper_wall]
 
     #buttons
     start_buttons = [["easy",430,215,"ASSESTS/240x70.png"],["medium",430,316,"ASSESTS/240x70.png"],["hard",430,417,"ASSESTS/240x70.png"]]
-
+    end_buttons = [["play_again",296,458,"ASSESTS/240x70.png"],["quit",569,458,"ASSESTS/240x70.png"]]
+    
     # NPC's
     house1_npcs = [["Cole",230,300,"ASSESTS/yellow.png"]]
-    house2_npcs = [["Folyd",300,300,"ASSESTS/pink.png"],["Mr. Blue",230,300,"ASSESTS/blue.png"]]
+    house2_npcs = [["Folyd",300,300,"ASSESTS/pink.png"],["Mr. Blue",230,300,"ASSESTS/blue-100.png"]]
+     
+    #flower
+    flower =[["flower",(WIDTH/2)-55,(HEIGHT/2)-107,"ASSESTS/Flower.png"]]
+    petal =[["petal",WIDTH/2,HEIGHT/2,"ASSESTS/Petal.png"]]
     
     #rooms
     homescreen = Room("homescreen","backgrounds/homescreen.png",[],f_border,[],start_buttons)
@@ -361,16 +351,19 @@ if True: # just to allow collapsing
     house3 = Room("house3","backgrounds/BG-house.png",[[500,635,100,15,"1"]],s_border,[],[])
     room2a = Room("room2a","backgrounds/BG-2-A.png",[[0,0,15,650,"1"],[1085,0,15,650,"2b"]],s_border,room2a_ws,[])
     room2b = Room("room2b","backgrounds/BG-2-B.png",[[0,0,15,650,"2a"],[1085,0,15,650,"3"]],s_border,room2b_ws,[])
-    room3  = Room("room3","backgrounds/BG-3.png",[[0,0,15,650,"2b"],[738,210,28,39,"4"]],s_border,room3_ws,[])
-    room4 = Room("room4","backgrounds/MAZE-BG.png",[[0,0,5,650,"3"],[1090,620,10,30,"5"]],f_border,create_maze(),[])
-    room5 = Room("room5","backgrounds/MAZE-BG.png",[[1085,0,15,650,"1"]],f_border,[],[])
-
+    room3  = Room("room3","backgrounds/BG-3.png",[[0,0,15,650,"2b"],[738,210,28,39,"4"]],s_border,room3_ws,petal)
+    # room 4 is in main loop
+    room5 = Room("room5","backgrounds/Final-BG.png",[[1085,0,15,650,"endscreen"]],f_border,[],flower)
+    endscreen = Room("endscreen","backgrounds/Asset 40.png",[],f_border,[],end_buttons)
+    
+    #spawn numbers =
     spawn_x_left = 50
     spawn_x_right = WIDTH - (100)
     spawn_x_center = WIDTH/2
     spawn_y_center = HEIGHT/2
     spawn_y_bottom = HEIGHT - 120
     spawn_y_top = 20
+    
     #Sound
     boss = pygame.mixer.Sound("Music/bossfight.wav")
     triumphant = pygame.mixer.Sound("Music/triumphant.wav")
@@ -385,9 +378,8 @@ if True: # just to allow collapsing
     triumphant.set_volume(0.7)
     chill.play(loops=30, maxtime=0, fade_ms=0)
     
-    
-    
-    
+    #Other
+    health_bar = HealthBar()
     clock = pygame.time.Clock()
     room = homescreen
     run = True
@@ -397,30 +389,57 @@ if True: # just to allow collapsing
 
 while run:
     clock.tick(FPS)
+    #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    ### music contorls        
-    
-    if room == homescreen:
+    if room == homescreen: # Homescreen programing
         mouse_pos = pygame.mouse.get_pos()
         mouse_pressed = pygame.mouse.get_pressed(num_buttons=3)
         for button in room.inter_list:
             if button.rect.collidepoint(mouse_pos) == True and mouse_pressed[0] == True:
-                difficulty = button.name
                 room = room1
-        draw_window(player,room)
+                if button.name == "hard":
+                    maze_num = 3
+                elif button.name == "medium":
+                    maze_num = 2
+                elif button.name == "easy":
+                    maze_num = 1
+                room4 = Room("room4","backgrounds/MAZE-BG.png",[[0,0,5,650,"3"],[1090,620,10,30,"5"]],f_border,create_maze(maze_num),[])
+        SCREEN.blit(room.image, (0,0))
+        pygame.display.update()
+    elif room == endscreen: # Endscreen programing w/ reset if "play again"
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_pressed = pygame.mouse.get_pressed(num_buttons=3)
+        for button in room.inter_list:
+            if button.rect.collidepoint(mouse_pos) == True and mouse_pressed[0] == True:
+                if button.name == "play_again":
+                    room = homescreen
+                    dialog_tally = 0
+                    health_bar.health = 0
+                    player = Player(spawn_x_center,spawn_y_center)
+                    room3  = Room("room3","backgrounds/BG-3.png",[[0,0,15,650,"2b"],[738,210,28,39,"4"]],s_border,room3_ws,petal)
+                    room5 = Room("room5","backgrounds/Final-BG.png",[[1085,0,15,650,"endscreen"]],f_border,[],flower)  
+                    if random.randint(1,2) == 1:
+                        chill = chill1
+                    else:
+                        chill = chill2
+                    chill.play(loops=30, maxtime=0, fade_ms=0)
+                    triumphant.stop()
+                else:
+                    run = False
+        SCREEN.blit(room.image, (0,0))
+        pygame.display.update() 
     else:
         keys_pressed = pygame.key.get_pressed()
-        if in_dialog == False: #check to start dialog 
+        if in_dialog == False: #Check to start dialog
             if keys_pressed[pygame.K_SPACE]==True:
                 for NPC in room.inter_list:
                     if NPC.react_rect.colliderect(player.rect):
-                        in_dialog,list,dialog_tally = dialog(dialog_tally,NPC.name)
+                        in_dialog,list,dialog_tally = dialog(dialog_tally,NPC,room)
                         counter = -1
-                        print(in_dialog)
-                        print(list)
                         break
+            #player movement
             player.move(keys_pressed,room)
             for door in room.door_list:
                 if door.rect.colliderect(player.rect):
@@ -430,8 +449,7 @@ while run:
             length = len(list)-1
             keys_pressed = pygame.key.get_pressed()
             if keys_pressed[pygame.K_SPACE]==True:
-                print("jere")
-                if counter<=(length-1):
+                if counter<=(length-1): 
                     counter += 1
                     SCREEN.blit(pygame.image.load(list[counter]),(0,500))
                     pygame.display.update()
